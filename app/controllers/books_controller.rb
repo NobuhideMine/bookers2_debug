26df51ed03cdf6a4f5dbff843395390556eecae6
NobuhideMine <141533543+NobuhideMine@users.noreply.github.com>
2023-09-29 08:34:40 +0000

    [Add] いいね/コメント機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,13 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
     @book = Book.new
     @books = Book.all
+    
     @user = current_user
   end
 
